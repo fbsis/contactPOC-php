0ba2ced16cab9383292280859f43abad20714866
Felipe Braga <contato@fbsis.com>
2021-04-03 17:39:15 -0300

    feat: Contacts model + factories + seeders

---
+++ database/factories/ContactsFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contacts;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Contacts::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_04_03_035702_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id')->start_from(256);
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->engine = 'InnoDB';
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
