34f774d095ebe5bb0ded324432bded9144f3fd3e
Felipe Braga <contato@fbsis.com>
2021-04-04 21:26:53 -0300

    fix: name of the user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\User::create([
-            "name" => "readUsers",
+            "name" => "readAddUsers",
             "email" => "read.add@test-poc.com",
             "role" => "read-add",
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
